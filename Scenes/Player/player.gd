5d59680997521745560c99cd8184381de0b7398c
Weekaaa <alyalwakeel9@gmail.com>
2025-02-05 21:49:37 +0200

    minor changes
    - Cleaned files
    - Got pick ups to work
    - Adjusted player movement

---
+++ Scenes/Player/player.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+var Strawberries: int = 0
+
+var speed: int = 150
+var _theta: float
+var _direction: Vector2
+@export var rotation_speed: float = TAU * 2
+
+func _process(_delta):
+	_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = _direction * speed
+	move_and_slide()
+
+
+func _physics_process(delta):
+	if _direction:
+		$AnimationPlayer.play("walk")
+		_theta = wrapf(atan2(_direction.y, _direction.x)- rotation, -PI, PI)
+		rotation += clamp(rotation_speed * delta, 0, abs(_theta)) * sign(_theta)
+	else:
+		$AnimationPlayer.stop()
+
+
+func _on_pick_up_range_body_entered(body):
+	if "collected" in body:
+		if body.get_parent().name == 'Strawberry':
+			Strawberries += 1
+			print(Strawberries)
+		body.collected()
