fa4993b7789eeeaa88d8bdb707080468ea1227f0
Weekaaa <alyalwakeel9@gmail.com>
2025-02-07 23:31:52 +0200

    started visuals
    displayed player strawberry count on screen

--- Scenes/Level/map.gd
+++ Scenes/Level/map.gd
@@ -1,23 +1,34 @@
 extends Node2D
 
 @onready var Strawberry: PackedScene = preload("res://Scenes/Fruits/strawberry.tscn")
+@onready var Cherry: PackedScene = preload("res://Scenes/Fruits/cherry.tscn")
+@onready var Grape: PackedScene = preload("res://Scenes/Fruits/grape.tscn")
 
 func _process(_delta):
-	pass
+	if int(%StrCount.text) != $Player.Strawberries:
+		%StrCount.text = str($Player.Strawberries)
 	
-func _spawn_fruit():
+func _spawn_fruit(fruit_type: PackedScene, fruit_name: String):
 	var min_spawn_area = $SpawnLocations/Marker2D.global_position
 	var max_spawn_area = $SpawnLocations/Marker2D2.global_position
 	var random_pos = Vector2(
 		randf_range(min_spawn_area.x, max_spawn_area.x),
 		randf_range(min_spawn_area.y, max_spawn_area.y)
 	)
-	var fruit = Strawberry.instantiate()
+	
+	var fruit = fruit_type.instantiate()
 	fruit.global_position = random_pos
-	$Strawberry.add_child(fruit)
+	
+	if fruit_name == 'Strawberry':
+		$Fruits/Strawberry.add_child(fruit)
+	elif fruit_name == 'Grape':
+		$Fruits/Grape.add_child(fruit)
+	elif fruit_name == 'Cherry':
+		$Fruits/Cherry.add_child(fruit)
+		
 	Globals.entities += 1
 	print(Globals.entities)
 
 func _on_strawberry_timer_timeout():
-	if Globals.entities <= 100:
-		_spawn_fruit()
+	if Globals.entities < 100:
+		_spawn_fruit(Strawberry, 'Strawberry')
