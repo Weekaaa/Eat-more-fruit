1d1261fe7da901754b68c0b88dcb14b53c428cd0
Aly Alwakeel <151021940+Weekaaa@users.noreply.github.com>
2025-02-13 18:15:33 +0200

    Cherry improvements
    - improved particles
    - added death of fruits nearby
    - need to fix bug with first 2 cherries spawning not working

--- Scenes/Fruits/cherry.gd
+++ Scenes/Fruits/cherry.gd
@@ -1,1 +1,10 @@
 extends Collectable
+
+
+func _on_hurt_box_body_entered(body: Node2D) -> void:
+	print('a ', body.get_parent().name, ' has exploded!')
+	if 'collected' in body:
+		body.collected()
+
+func blasted():
+	queue_free()
