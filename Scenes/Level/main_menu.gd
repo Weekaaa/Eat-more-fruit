74ffec14fc068d1b8b0218ac08e7cdc726646693
Weekaaa <alyalwakeel9@gmail.com>
2025-02-26 13:27:29 +0200

    Improved ghost behavior
    -ghosts now follow fruits in order of which entered its' pickup range first
    -movement overall is smoother
    -added fun button in main menu

--- Scenes/Level/main_menu.gd
+++ Scenes/Level/main_menu.gd
@@ -1,8 +1,11 @@
 extends Control
 
+var active: bool = false
+
 func _process(_delta):
 	for fruit in $FallingFruits.get_children():
-		fruit.get_child(0).progress += randf_range(1.3, 1.9)
+		if fruit.name != 'Huge':
+			fruit.get_child(0).progress += randf_range(1.3, 1.9)
 
 
 func _on_start_game_button_button_up():
@@ -10,3 +13,13 @@ func _on_start_game_button_button_up():
 	$AnimationPlayer.play("FadeIn")
 	await $AnimationPlayer.animation_finished
 	get_tree().change_scene_to_file("res://Scenes/Level/Map.tscn")
+
+
+func _on_watermelon_button_button_up():
+	if not active:
+		active = true
+		var tween = create_tween()
+		tween.tween_property($FallingFruits/Huge/Pa, "progress_ratio", 1.0, 5).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
+		await tween.finished
+		$FallingFruits/Huge/Pa.progress_ratio = 0.0
+		active = false
--- Scenes/Player/ghost.gd
+++ Scenes/Player/ghost.gd
@@ -2,19 +2,28 @@ extends CharacterBody2D
 
 @onready var swallow = preload("res://Assets/Ui/Emote/emote1.png")
 @onready var hungry = preload("res://Assets/Ui/Emote/emote2.png")
-@export var move_speed: float = 40
+@export var move_speed: float = 60
 var wander_radius: Vector2 = Vector2(275, 150) 
-var current_tween: Tween
+var tween: Tween
 var in_range = []
+var to_follow = []
+var last_pos: Vector2 
+var following = false
 
 func _ready():
 	_pick_new_target()
 	$WanderTimer.wait_time = randf_range(3.0, 5.0)
 	$WanderTimer.start()
+	last_pos = global_position
 
 func _process(_delta):
 	if not in_range:
 		$Expression.texture = hungry
+	
+	if not following and not to_follow.is_empty():
+		_follow_fruit(to_follow.pop_front())
+		%WanderTimer.stop()
+		$Expression.texture = swallow
 
 func _pick_new_target():
 	var target = Vector2(
@@ -22,51 +31,58 @@ func _pick_new_target():
 		randf_range(-wander_radius.y, wander_radius.y)
 	)
 	
-	if current_tween:
-		current_tween.kill()
+	if tween:
+		tween.kill()
 	
-	current_tween = create_tween()
-	current_tween.tween_property(
+	tween = create_tween()
+	tween.tween_property(
 		self, 
 		"global_position", 
 		target, 
 		global_position.distance_to(target) / move_speed
 	).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
 	
-	current_tween.finished.connect(_on_movement_finished)
+	tween.finished.connect(_on_movement_finished)
 
 func _follow_fruit(body):
+	following = true
 	var target = body.global_position
-	if current_tween:
-		current_tween.kill()
+	if tween:
+		tween.kill()
 	
-	current_tween = create_tween()
-	current_tween.tween_property(
+	tween = create_tween()
+	tween.tween_property(
 		self, 
 		"global_position", 
 		target, 
 		global_position.distance_to(target) / move_speed
 	).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
 	
-	current_tween.finished.connect(_on_movement_finished)
+	tween.finished.connect(_on_follow_finished.bind(body))
+
+func _on_follow_finished(body):
+	if is_instance_valid(body) and "collected" in body:
+		_on_collect_area_body_entered(body)
+	following = false
+	_on_movement_finished()
 
 func _on_movement_finished():
-	$WanderTimer.start(randf_range(1.0, 2.0))
+	$WanderTimer.start(randf_range(1.5, 3.5))
 
 func _on_wander_timer_timeout():
 	_pick_new_target()
 
 func _on_pick_up_range_body_entered(body):
-	%WanderTimer.stop()
-	_follow_fruit(body)
-	$Expression.texture = swallow
+	to_follow.append(body)
 	in_range.append(body)
 
 func _on_pick_up_range_body_exited(body):
 	in_range.erase(body)
+	to_follow.erase(body)
 
 func _on_collect_area_body_entered(body):
 	if "collected" in body:
+		following = false
 		if body.get_parent().name == 'Strawberry':
 			Globals.Strawberries += body.gain
 		elif body.get_parent().name == 'Grape':
